147531f742752bf50c4cf6c25d249bf6901c6499
Sello Fotoyi <selloclinton@gmail.com>
2020-05-24 19:50:03 +0200

    reviewed access to entities(eg particular track name). This will be useful in logic

--- flex_muscle.js
+++ flex_muscle.js
@@ -1,18 +1,28 @@
-window.onload = function(){
-	var trackList = new Array();	//size not known yet
+window.onload = function(){ 
+
+	/*var nextTrack = document.getElementById("next");
+	nextTrack.style.color = 'black';
+	
+	var trackNums = document.getElementsByClassName("listTrackNumber");
+	
+	trackNums[2].style.color = 'red';*/
+	
+ 	var trackList = new Array();	//size not known yet
+	var trackNums = document.getElementsByClassName("listTrackName");
+	trackNums[7].innerHTML = "Changed track name";
 	
 	trackList[0] = "Belvedere.mp3";
-	trackList[1] = "Blue Wednesday.mp3"
-	trackList[2] = "Favorite Drive.mp3"
-	trackList[3] = "Fly Like Birds.mp3"
-	trackList[4] = "Grizzly Peak.mp3"
-	trackList[5] = "kensington.mp3"
-	trackList[6] = "middle school.mp3"
-	trackList[7] = "Run away.mp3"
-	trackList[8] = "Soulful.mp3"
-	trackList[9] = "SUmmer Daze(Dukesouls Mellow Touch).mp3";
+	trackList[1] = "Blue Wednesday.mp3";
+	trackList[2] = "Favorite Drive.mp3";
+	trackList[3] = "Fly Like Birds.mp3";
+	trackList[4] = "Grizzly Peak.mp3";
+	trackList[5] = "kensington.mp3";
+	trackList[6] = "middle school.mp3";
+	trackList[7] = "Run away.mp3";
+	trackList[8] = "Soulful.mp3";
+	trackList[9] = "Summer Daze(Dukesouls Mellow Touch).mp3";
 	
-	var currentTrack = 0;	//@start of array
+	var currentTrack = 0;	// @start of array
 	var trackSrc = document.getElementById("trackSource");
 	
 	//assign a track from track list to track src
@@ -26,11 +36,11 @@ window.onload = function(){
 	var shuffleMusic = document.getElementById("shufl");
 	
 	//add event listeners
-	nextTrack.addEventListener("click", next()));
-	prev.addEventListener("click", previous());
-	stopMusic.addEventListener("click", stop());
-	loopMusic.addEventListener("click", loop());
-	shuffleMusic.addEventListener("click", shuffle());
+	nextTrack.addEventListener("click", next);
+	prev.addEventListener("click", previous);
+	stopMusic.addEventListener("click", stop);
+	loopMusic.addEventListener("click", loop);
+	shuffleMusic.addEventListener("click", shuffle);
 	
 	//event functions
 	function next(){
@@ -59,7 +69,7 @@ window.onload = function(){
 		
 	}
 	//event functions: end
-	
+ 
 	
 
 };
